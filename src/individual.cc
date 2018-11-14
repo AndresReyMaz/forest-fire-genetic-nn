bb74f03d0553e14d76992cd4e4b9c6ab07eccf76
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-11-14 17:19:32 -0600

    Write setters for Individual.

--- src/individual.cc
+++ src/individual.cc
@@ -13,3 +13,25 @@ int Individual::to_value(int start, int end) const {
   }
   return ret;
 }
+
+void Individual::set_value(int start, int end, int value) {
+  for (int i = end; i >= start; --i) {
+    bit_vector[i] = value % 2;
+    value /= 2;
+  }
+}
+
+void Individual::set_training_time(int training_time) {
+  assert(training_time >= MIN_TRAINING_TIME and training_time <= MAX_TRAINING_TIME);
+  Individual::set_value(TRAINING_TIME_START, TRAINING_TIME_END, training_time - MIN_TRAINING_TIME);
+}
+
+void Individual::set_learning_rate(int learning_rate) {
+  assert(learning_rate >= MIN_LEARNING_RATE and learning_rate <= MAX_LEARNING_RATE);
+  Individual::set_value(LEARNING_RATE_START, LEARNING_RATE_END, learning_rate - MIN_LEARNING_RATE);
+}
+
+void Individual::set_momentum(int momentum) {
+  assert(momentum >= MIN_MOMENTUM and momentum <= MAX_MOMENTUM);
+  Individual::set_value(MOMENTUM_START, MOMENTUM_END, momentum - MIN_MOMENTUM);
+}
--- src/individual.h
+++ src/individual.h
@@ -1,6 +1,9 @@
 #ifndef GENETIC_INDIVIDUAL_H
 #define GENETIC_INDIVIDUAL_H
 
+#include <assert.h>
+#include <bitset>
+
 const int N_BITS = 57;
 
 class Individual {
@@ -24,24 +27,38 @@ class Individual {
     return to_value(HIDDEN_LAYERS_START, HIDDEN_LAYERS_END);
   }
 
-  int get_neurons_for_layer(int layer) {
+  int get_neurons_for_layer(int layer) const {
     assert(layer <= get_hidden_layers());
     return to_value(NUMBER_NEURONS_START + NUMBER_NEURONS_LENGTH * (layer - 1),
 		    NUMBER_NEURONS_START + NUMBER_NEURONS_LENGTH * layer - 1) + MIN_NUMBER_NEURONS;
   }
 
-  int get_training_time() {
+  int get_training_time() const {
     return to_value(TRAINING_TIME_START, TRAINING_TIME_END) + MIN_TRAINING_TIME;
   }
 
-  int get_learning_rate() {
+  int get_learning_rate() const {
     return to_value(LEARNING_RATE_START, LEARNING_RATE_END) + MIN_LEARNING_RATE;
   }
 
-  int get_momentum() {
+  int get_momentum() const {
     return to_value(MOMENTUM_START, MOMENTUM_END) + MIN_MOMENTUM;
   }
 
+  int get_neurons() const {
+    // Note: this gets all the neuron information as a non-normalized number.
+    return to_value(HIDDEN_LAYERS_START, NUMBER_NEURONS_END);
+  }
+  
+  void set_training_time(int training_time);
+  void set_learning_rate(int learning_rate);
+  void set_momentum(int momentum);
+  void set_neurons(int value) {
+    set_value(HIDDEN_LAYERS_START, NUMBER_NEURONS_END, value);
+  }
+
+  Individual() { };
+  
   Individual(const Individual &other) {
     bit_vector = std::bitset<N_BITS>(other.get_vector().to_ullong());
   }
@@ -54,7 +71,7 @@ class Individual {
   static const int NUMBER_NEURONS_LENGTH = 7;
   static const int NUMBER_NEURONS_END = NUMBER_NEURONS_START + NUMBER_NEURONS_LENGTH * MAX_HIDDEN_LAYERS - 1;
   static const int TRAINING_TIME_START = NUMBER_NEURONS_END + 1;
-  static const int TRAINING_TIME_LENGTH = 13;
+  static const int TRAINING_TIME_LENGTH = 8;
   static const int TRAINING_TIME_END = TRAINING_TIME_START + TRAINING_TIME_LENGTH - 1;
   static const int LEARNING_RATE_START = TRAINING_TIME_END + 1;
   static const int LEARNING_RATE_LENGTH = 13;
@@ -64,6 +81,8 @@ class Individual {
   static const int MOMENTUM_END = MOMENTUM_START + MOMENTUM_LENGTH - 1;
 
   int to_value(int start, int end) const;
+
+  void set_value(int start, int end, int value);
 };
 
 #endif
